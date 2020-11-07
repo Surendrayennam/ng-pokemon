bf110b5ba91cbbe94fd53f2c4592cb5fc622a24e
Surendranath reddy Yennam <surendranathreddyyennam@MacBook-Pro.local>
2020-11-06 23:00:45 -0500

    service added base url

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,19 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
+  private readonly baseUrl = 'https://pokeapi.co/api/v2';
   constructor(private http: HttpClient) { }
 
-public getAllPokemanDetails(){
-  return this.http.get('https://pokeapi.co/api/v2/pokemon?limit=20');
-}
+  public getAllPokemanDetails() {
+    return this.http.get(this.baseUrl + '/pokemon?limit=20');
+  }
 
-public getImageDetails(id: number){
-  return this.http.get('https://pokeapi.co/api/v2/pokemon/' + id);
-}
-
+  public getImageDetails(id: number) {
+    return this.http.get(this.baseUrl + '/pokemon/' + id);
+  }
 }
